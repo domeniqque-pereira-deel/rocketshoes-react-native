5d06eb113edba50b66b67a8625dca9471ba645d5
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-10-05 10:24:19 -0400

    chore: update readme and api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
+<<<<<<< HEAD
     'https://my-json-server.typicode.com/Domeniqque/rocketshoes-react-native/products',
+=======
+    'https://my-json-server.typicode.com/Domeniqque/rocketshoes-react-native',
+>>>>>>> chore: update readme and api url
 });
 
 export default api;
